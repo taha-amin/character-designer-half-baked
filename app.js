af7886ab08638d4ba43643b44a2bf04162715a8e
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-03-09 12:46:58 -0800

    added states for head, middle, and bottom and set them all to 0. Got the values of the head/middle/bottom dropdowns and added them in their respective event listeners. Incremented each one as well. In func displayStats I used my report element to report to the user how many times the body, middle, and bottom changed, then added that func to each event listener. For func displayCatchphrases, I removed the classlist first, then created a for loop and appended the h2tag.

--- app.js
+++ app.js
@@ -11,38 +11,53 @@ const catchphraseInput = document.getElementById('catchphrase-input');
 const catchphraseButton = document.getElementById('catchphrase-button');
 
 // set state for how many times the user changes the head, middle, and bottom
+let headCount = 0;
+let middleCount = 0; 
+let bottomCount = 0;
+
 // set state for all of the character's catchphrases
+let catchPhraseCount = [];
 
 headDropdown.addEventListener('change', () => {
     // get the value of the head dropdown
+    headEl.textContent = headDropdown.value;
 
     // increment the head change count state
-    
+    headCount++;
+
     // update the dom for the head (use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
 
+
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
 });
 
 
 middleDropdown.addEventListener('change', () => {
     // get the value of the middle dropdown
+    middleEl.textContent = middleDropdown.value;
 
     // increment the middle change count state
-    
+    middleCount++;
+
     // update the dom for the middle (NOTE: use style.backgroundImage on the middleEl div instead of trying to set the .src -- it's NOT an img tag!)
 
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
 });
 
 
 bottomDropdown.addEventListener('change', () => {
     // get the value of the bottom dropdown
+    bottomEl.textContent = bottomDropdown.value;
 
     // increment the bottom change count state
-    
+    bottomCount++;
+
     // update the dom for the bottom (NOTE use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
 
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
 });
 
 catchphraseButton.addEventListener('click', () => {
@@ -58,16 +73,22 @@ catchphraseButton.addEventListener('click', () => {
 
 function displayStats() {
     // text content of the reportEl to tell the user how many times they've changed each piece of the state
+    reportEl.textContent = `You changed the head ${headCount} times, the middle ${middleCount} times, and the bottom ${bottomCount} times.`;
 }
 
 function displayCatchphrases() {
     // clear out the DOM for the currently displayed catchphrases
+    catchphrasesEl.classList.remove();
 
     // loop through each catchphrase in state
-   
+    for(let catchphrase of catchphrases) {
+        const h2Tag = document.createElement('h2');
+
     // and for each catchphrase
-    
     // create an HTML element with the catchphrase as its text content
-    
     // and append that HTML element to the cleared-out DOM
+
+        h2Tag.textContent = catchphraseInput;
+        catchphrasesEl.append(h2Tag);
+    }
 }
